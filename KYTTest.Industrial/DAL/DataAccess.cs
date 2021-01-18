936f3e85dd5fad92b855e6c0899d46da3d3eaf9d
wei yuan <737571078@qq.com>
2021-01-18 17:40:13 +0800

    //增加Mysql连接类

---
+++ KYTTest.Industrial/DAL/DataAccess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
+
+namespace KYTTest.Industrial.DAL
+{
+    class DataAccess
+    {
+        string TestDataSaveConfig = ConfigurationManager.ConnectionStrings["TestDataSave_config"].ToString();
+
+        MySqlConnection conn;//连接数据库
+        MySqlCommand cmd;//数据库操作函数
+
+        private void Dispose()
+        {
+
+        }
+
+    }
+}
---
+++ KYTTest.communication/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KYTTest.communication
+{
+    public class Class1
+    {
+    }
+}
