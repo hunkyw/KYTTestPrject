9cb7e10379fff0622d08f8546fedb58acc539b17
wei yuan <737571078@qq.com>
2021-01-19 15:55:17 +0800

    //

--- KYTTest.Industrial/BLL/IndustrialBLL.cs
+++ KYTTest.Industrial/BLL/IndustrialBLL.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,19 +14,29 @@ namespace KYTTest.Industrial.BLL
         /// <summary>
         /// 获取串口设备信息
         /// </summary>
-        public SerialInfo InitSerialInfo()
+        public DataResult<SerialInfo> InitSerialInfo()
         {
-            SerialInfo si = new SerialInfo();
+            DataResult<SerialInfo> result = new DataResult<SerialInfo>();
+            result.State = false;
 
             try
             {
+                SerialInfo si = new SerialInfo();
+                si.PortName = ConfigurationManager.AppSettings["port"].ToString();
+                si.baudRate = int.Parse(ConfigurationManager.AppSettings["baud"].ToString());
+                si.DataBit = int.Parse(ConfigurationManager.AppSettings["data_bit"].ToString());
+                si.Parity = (Parity)Enum.Parse(typeof(Parity), ConfigurationManager.AppSettings["parity"].ToString(), true);
+                si.StopBits = (StopBits)Enum.Parse(typeof(Parity), ConfigurationManager.AppSettings["stop_bits"].ToString(), true);
 
+                result.State = true;
+                result.Data = si;
             }
             catch (Exception ex)
             {
-                throw ex;
+                result.Mseeage = ex.Message;
+
             }
-            return si;
+            return result;
         }
     }
 }
